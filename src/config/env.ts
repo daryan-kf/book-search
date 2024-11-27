0c780ed9b5fe3895d4402f7128f774432588197e
daryan-kf <daryankamalifar@gmail.com>
2024-11-27 11:09:06 +0100

    configure the ENV to be easly imported & used

---
+++ src/config/env.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+// Load the environment variables
+dotenv.config();
+
+// TODO: add a validation script for required ENV variables later.
+
+// Export process.env for use throughout the project
+export default process.env;
